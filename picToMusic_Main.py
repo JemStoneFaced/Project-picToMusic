a87b2872cfc3d2b6347e8008e55af8c0aa985196
JemStoneFaced <130704063+JemStoneFaced@users.noreply.github.com>
2023-04-20 16:13:16 -0400

    Update picToMusic_Main.py

--- picToMusic_Main.py
+++ picToMusic_Main.py
@@ -1,6 +1,23 @@
 #-------------------------------------------
-#Program: picToMusic_Main
-#Author: James Dunne
-#Date: 4.20.2023
-#Version: 0.1a
+# Program: picToMusic_Main
+# Author: James Dunne
+# Date: 4.20.2023
+# Version: 0.1a
 #-------------------------------------------
+
+# Keep track of settings
+# Handle user input
+
+# Import libraries
+
+# aquire either user images or image from sample img folder
+# see if we have an image
+
+# save img in temps file
+# ask computer to view image
+# grab hue from HSV
+# Separate the hues and convert to frequency
+# generic song save
+
+# make effected version of song
+# save final song and make a midi version
